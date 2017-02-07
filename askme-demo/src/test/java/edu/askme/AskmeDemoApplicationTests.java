944898bb3d6e20691baf97c6c1164de37658d353
tarek <tarek.nawara@gmail.com>
2017-02-07 18:19:02 +0200

    initial commit

---
+++ askme-demo/src/test/java/edu/askme/AskmeDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.askme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AskmeDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
