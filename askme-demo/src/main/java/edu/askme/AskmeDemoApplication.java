fea252a3e55324e6ce4729ffa70a1c4410926095
tarek <tarek.nawara@gmail.com>
2017-02-07 18:16:43 +0200

    initial commit

---
+++ askme-demo/src/main/java/edu/askme/AskmeDemoApplication.java
@@ -0,0 +1,12 @@
+package edu.askme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskmeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskmeDemoApplication.class, args); 
+	}
+}
